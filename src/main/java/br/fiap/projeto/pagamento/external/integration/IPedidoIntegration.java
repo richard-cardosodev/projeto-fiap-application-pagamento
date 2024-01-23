4262df37153ef8d3c1c7de64975f8d7b60d7da61
Richard Cardoso <richard.166@hotmail.com>
2024-01-23 14:36:47 -0300

    wip: deploy

--- src/main/java/br/fiap/projeto/pagamento/external/integration/IPedidoIntegration.java
+++ src/main/java/br/fiap/projeto/pagamento/external/integration/IPedidoIntegration.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 
 
-@FeignClient(value="pedidoIntegration", url = "http://localhost:${server.port}/pedidos")
+@FeignClient(value="pedidoIntegration", url = "http://${pedido.host}/pedidos")
 public interface IPedidoIntegration {
 
     @RequestMapping(method = RequestMethod.GET, value = "/busca-recebidos")
