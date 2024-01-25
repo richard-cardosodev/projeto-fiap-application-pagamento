e29b419ac928b2961f27d64562cbed3eaa72e7b5
Richard Cardoso <richard.166@hotmail.com>
2024-01-25 19:05:29 -0300

    test: wip

--- src/test/java/br/fiap/projeto/pagamento/bdd/acceptance/PagamentoCucumberRunner.java
+++ src/test/java/br/fiap/projeto/pagamento/bdd/acceptance/PagamentoCucumberRunner.java
@@ -1,12 +1,7 @@
 package br.fiap.projeto.pagamento.bdd.acceptance;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-
-@RunWith(Cucumber.class) //junit4 integrado ao 5, Cucumber n funciona direto cm o junit5 //Extends no junit5
-@CucumberOptions(features = "classpath:features")
+//@RunWith(Cucumber.class) //junit4 integrado ao 5, Cucumber n funciona direto cm o junit5 //Extends no junit5
+//@CucumberOptions(features = "classpath:features")
 public class PagamentoCucumberRunner {
 
 }
